0fddc5380920e29434f883378bca07e364395e67
hathanhtinh10102004 <0387232596tinh@gmail.com>
2023-10-30 15:19:06 +0700

    hienthhidanhsachUser

---
+++ src/main/java/model/Admin.java
@@ -0,0 +1,51 @@
+package model;
+
+public class Admin {
+    private int Id;
+    private String Name;
+    private String Email;
+    private int PhoneNumber;
+
+    public Admin(int id, String name, String email, int phoneNumber) {
+        Id = id;
+        Name = name;
+        Email = email;
+        PhoneNumber = phoneNumber;
+    }
+
+    public Admin() {
+
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public int getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        PhoneNumber = phoneNumber;
+    }
+}
